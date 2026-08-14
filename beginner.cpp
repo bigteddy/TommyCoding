8079b9b4518500dc2695c5199252c5679c621579
Yuntao <yuntao2006@gmail.com>
2026-08-14 14:52:09 -0400

    uncomment the line 10 - 32

--- beginner.cpp
+++ beginner.cpp
@@ -7,29 +7,29 @@ int main () {
     printf("%.3f\n", 8.0/5.0);
     printf("%.8f\n", 1+2*sqrt(3)/(5-0.1));
 
-    // char a[100] = "abcd";
-    // scanf("%d%d", &a, &b);
-    // printf("output is: %s\n", a);
+    char a[100] = "abcd";
+    scanf("%d%d", &a, &b);
+    printf("output is: %s\n", a);
     
-    // char name[100];
+    char name[100];
 
-    // printf("Who are you? ");
-    // // scanf("%99s", name);   // Input String
-    // fgets(name, 15, stdin);
-    // name[strcspn(name, "\n")] = '\0';  // remove newline
-    // printf("Hello %s ^_^", name);
+    printf("Who are you? ");
+    // scanf("%99s", name);   // Input String
+    fgets(name, 15, stdin);
+    name[strcspn(name, "\n")] = '\0';  // remove newline
+    printf("Hello %s ^_^", name);
 
-    // int n = 555;
-    // printf("%06d", n);
+    int n = 555;
+    printf("%06d", n);
 
-    // const double pi = acos(-1.0);
-    // double r, h, s1, s2, s;
-    // scanf("%lf%lf", &r, &h);
+    const double pi = acos(-1.0);
+    double r, h, s1, s2, s;
+    scanf("%lf%lf", &r, &h);
     
-    // s1 = pi*r*r;
-    // s2 = 2*pi*r*h;
-    // s = s1*2.0 + s2;
-    // printf("Area = %.3f\n", s);
+    s1 = pi*r*r;
+    s2 = 2*pi*r*h;
+    s = s1*2.0 + s2;
+    printf("Area = %.3f\n", s);
 
     // int n;
     // scanf("%d", &n);
